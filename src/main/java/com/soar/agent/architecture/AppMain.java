b4ff27345bf8835cb5e856921c12441fda7a2159
Reza <reza.amini.b@gmail.com>
2022-09-01 14:38:39 +0100

    - add swing gui - fix maven build to have portable executable jar with spring boot

---
+++ src/main/java/com/soar/agent/architecture/AppMain.java
@@ -0,0 +1,42 @@
+package com.soar.agent.architecture;
+
+import java.io.IOException;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
+
+import org.jsoar.debugger.util.SwingTools;
+
+/**
+ * Hello world!
+ *
+ */
+public class AppMain extends JPanel {
+
+    public AppMain() throws IOException {
+
+    }
+    public static void main(String[] args) {
+        initSwing();
+    }
+
+    private static void initSwing(){
+        SwingTools.initializeLookAndFeel();
+        SwingUtilities.invokeLater(() ->
+        {
+            JFrame f = new JFrame();
+            f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+            try {
+                AppMain content = new AppMain();
+                f.setContentPane(content);
+                f.setSize(640, 640);
+                f.setVisible(true);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+    }
+}
