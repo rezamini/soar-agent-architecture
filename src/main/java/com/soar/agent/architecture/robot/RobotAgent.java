ec6b7dca1b7904e0ba926e825389d5b1986bc0f3
Reza <reza.amini.b@gmail.com>
2022-09-30 16:26:39 +0100

    - move agent to landmark while avoiding obstacles. if they exist on the way. This is a new rule production file and its might change. specifically the way the random direction is proposed, if obstacle exists. For now it is static for testing purposes.

--- src/main/java/com/soar/agent/architecture/robot/RobotAgent.java
+++ src/main/java/com/soar/agent/architecture/robot/RobotAgent.java
@@ -73,7 +73,9 @@ public class RobotAgent {
             // source = new
             // File(getClass().getResource("/rules/move-forward-prefer-current-direction.soar").toURI());
             // source = new File(getClass().getResource("/rules/move-to-landmark-1.0.soar").toURI());
-            source = new File(getClass().getResource("/rules/move-to-landmark-1.1.soar").toURI());
+            // source = new File(getClass().getResource("/rules/move-to-landmark-1.1.soar").toURI());
+            source = new File(getClass().getResource("/rules/move-to-landmark-2.0.soar").toURI());
+
 
             // source = new File(getClass().getResource("/rules/move-random.soar").toURI());
             // source = new
