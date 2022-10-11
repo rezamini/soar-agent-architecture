ea1ce8813cbe5c27d50c6f76482d27e06f3a6ae7
rezamini <reza.amini.b@gmail.com>
2022-10-11 17:48:11 +0100

    - added memory enum to keep track of memory name.. changes in one single file

---
+++ src/main/java/com/soar/agent/architecture/enums/MemoryEnum.java
@@ -0,0 +1,31 @@
+package com.soar.agent.architecture.enums;
+
+enum MemoryEnum {
+  //General
+  BASIC_NAME("name"),
+  IDENTITY("self"),
+  MINIMUM_BOUNDING_BOX("mbb"),
+  POSITION("pose"),
+  POSITION_X("x"),
+  POSITION_Y("y"),
+  YAW("yaw"),
+  DISTANCE("distance"),
+  DIRECTION_COMMAND("direction-command"),
+  //Landmark Memory Enums  
+  LANDMARK_MAIN("landmarks"),
+  LANDMARK_SUB("landmark");
+
+  private String name;
+
+  private MemoryEnum(String name){
+    this.name = name;
+  }
+
+public String getName() {
+    return name;
+}
+
+public void setName(String name) {
+    this.name = name;
+}
+}
