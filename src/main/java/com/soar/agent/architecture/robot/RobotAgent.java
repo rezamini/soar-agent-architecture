87a85be79d09eeb9861ef4c5b0534693eb65a8ae
rezamini <reza.amini.b@gmail.com>
2022-11-26 23:33:17 +0000

    - added a new soar rule file to combine both... the epmem & smem rules and learning.

--- src/main/java/com/soar/agent/architecture/robot/RobotAgent.java
+++ src/main/java/com/soar/agent/architecture/robot/RobotAgent.java
@@ -91,11 +91,14 @@ public class RobotAgent {
             // File(getClass().getResource("/rules/move-to-landmark-3.0.soar").toURI());
             // source = new
             // File(getClass().getResource("/rules/move-to-landmark-3.0-rl.soar").toURI());
-            source = new
-            File(getClass().getResource("/rules/move-to-landmark-3.0-epmem.soar").toURI());
+            // source = new
+            // File(getClass().getResource("/rules/move-to-landmark-3.0-epmem.soar").toURI());
 
             // source = new
             // File(getClass().getResource("/rules/move-to-landmark-3.0-smem.soar").toURI());
+
+            source = new
+            File(getClass().getResource("/rules/move-to-landmark-3.0-smem-epmem.soar").toURI());
             // source = new
             // File(getClass().getResource("/rules/main/main-default.soar").toURI());
 
