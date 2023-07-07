c1b87ebabc3010c3fada0da48e01e8fc6902ebaf
Reza <reza.amini.b@gmail.com>
2023-07-07 13:23:04 +0100

    - added a new class to test direction movements... of the agent without obstacles.

---
+++ src/test/java/com/soar/agent/architecture/DirectionMovementTest.java
@@ -0,0 +1,11 @@
+package com.soar.agent.architecture;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+public class DirectionMovementTest {
+    
+}
