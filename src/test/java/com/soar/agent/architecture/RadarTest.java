e1f76afe161818d77c89defb38222365ee2b24dc
Reza <reza.amini.b@gmail.com>
2023-07-14 11:38:10 +0100

    - added test cases for radar battery percentage... after one or multiple movements. The objective is to ensure that the agent's radar accurately updates the battery percentage based on the energy consumed during movement. This test will check if the battery percentage decreases by the expected amount after the agent moves.

--- src/test/java/com/soar/agent/architecture/RadarTest.java
+++ src/test/java/com/soar/agent/architecture/RadarTest.java
@@ -1,11 +1,20 @@
 package com.soar.agent.architecture;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import com.soar.agent.architecture.enums.DirectionEnum;
 import com.soar.agent.architecture.events.UtilityResponder;
 import com.soar.agent.architecture.loader.MapLoader;
 import com.soar.agent.architecture.robot.Robot;
@@ -13,6 +22,10 @@ import com.soar.agent.architecture.robot.RobotAgent;
 import com.soar.agent.architecture.world.World;
 import com.soar.agent.architecture.world.WorldPanel;
 
+// @RunWith( SpringRunner.class ) //junit 4
+@ExtendWith(SpringExtension.class) // junit 5, jupiter
+// @SpringBootTest(properties = "map.file.name=map-test.txt")
+@SpringBootTest
 public class RadarTest {
     @Autowired
     public MapLoader mapLoader;
@@ -52,5 +65,103 @@ public class RadarTest {
         robot.setSpeed(0.5);
     }
 
+    @Test
+    @DisplayName("Verify the battery percentage of the agent after performing one movement. " +
+            "The objective is to ensure that the agent's radar accurately updates the battery percentage based on the" +
+            "energy consumed during movement. This test will check if the battery percentage decreases by the expected amount after the agent moves.")
+    public void testAgentRadarBatteryAfterOneMovement() throws IOException {
+        StringBuilder sb = new StringBuilder();
+        sb.append(" ").append("\n")
+                .append("        ").append("\n")
+                .append("    R   ").append("\n")
+                .append("        ");
+
+        createNewWorld(sb.toString());
+
+        // set the yaw to east direction
+        robot.setYaw(Math.toRadians(DirectionEnum.EAST.getAngle()));
+
+
+        // expected battery, original battery before movement - deduction
+        double expectedBattery = robot.getRadarBattery() - robot.getBatteryDeduction();
+
+        // move the agent
+        robot.updateAndMove(0);
+
+        //get the battery result
+        double batteryResult = robot.getRadarBattery();        
+        
+
+        // make sure the agent battery result and expected result are same
+        assertEquals(batteryResult, expectedBattery);
+    }
+
+
+    @Test
+    @DisplayName("Verify the battery percentage of the agent after performing two movements. " +
+            "The objective is to ensure that the agent's radar accurately updates the battery percentage based on the" +
+            "energy consumed during movement. This test will check if the battery percentage decreases by the expected amount after the agent moves.")
+    public void testAgentRadarBatteryAfterTwoMovements() throws IOException {
+        StringBuilder sb = new StringBuilder();
+        sb.append(" ").append("\n")
+                .append("        ").append("\n")
+                .append("    R   ").append("\n")
+                .append("        ");
+
+        createNewWorld(sb.toString());
+
+        // set the yaw to east direction
+        robot.setYaw(Math.toRadians(DirectionEnum.EAST.getAngle()));
+
+        // original battery percentage before any movement
+        double originalBattery = robot.getRadarBattery();
+
+        // move the agent 2 times
+        robot.updateAndMove(0);
+        robot.updateAndMove(0);
+
+        //get the battery result
+        double batteryResult = robot.getRadarBattery();        
+        
+        //expected battery after all movements
+        double expectedBattery = originalBattery - ( robot.getBatteryDeduction() * 2);
+
+        // make sure the agent battery result and expected result are same
+        assertEquals(batteryResult, expectedBattery);
+    }   
     
+
+    @Test
+    @DisplayName("Verify the battery percentage of the agent after performing three movements. " +
+            "The objective is to ensure that the agent's radar accurately updates the battery percentage based on the" +
+            "energy consumed during movement. This test will check if the battery percentage decreases by the expected amount after the agent moves.")
+    public void testAgentRadarBatteryAfterThreeMovements() throws IOException {
+        StringBuilder sb = new StringBuilder();
+        sb.append(" ").append("\n")
+                .append("         ").append("\n")
+                .append("    R    ").append("\n")
+                .append("         ");
+
+        createNewWorld(sb.toString());
+
+        // set the yaw to east direction
+        robot.setYaw(Math.toRadians(DirectionEnum.EAST.getAngle()));
+
+        // original battery percentage before any movement
+        double originalBattery = robot.getRadarBattery();
+
+        // move the agent 3 times
+        robot.updateAndMove(0);
+        robot.updateAndMove(0);
+        robot.updateAndMove(0);
+
+        //get the battery result
+        double batteryResult = robot.getRadarBattery();        
+        
+        //expected battery after all movements
+        double expectedBattery = originalBattery - ( robot.getBatteryDeduction() * 3);
+
+        // make sure the agent battery result and expected result are same
+        assertEquals(batteryResult, expectedBattery);
+    }     
 }
